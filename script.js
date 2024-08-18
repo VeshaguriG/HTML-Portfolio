f6333c840ce69caae8450f0316cea9c0fce3e13b
VeshaguriG <91211595+VeshaguriG@users.noreply.github.com>
2024-08-18 09:58:16 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const bdy = document.getElementsByClassName("bdy")
+let btn = document.getElementsByClassName("btn")
+
+
+btn = function() {
+    bdy.style.background = "green"
+}
+
+onclick="btn()"
